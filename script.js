aaba2a569e47d4ea78de6e7d40e1b5f45a674e86
= <=>
2024-01-23 00:10:31 +0530

    Implement Throttling concept.

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function getData1(e) {
 input.addEventListener("keyup", getData1);
 
 
-
 // Debouncing way
 function debounce(callback, minimumDelay) {
     var id;
@@ -29,3 +28,67 @@ var specailFun1 = debounce(getData2, 1000);
 
 input.addEventListener("keyup", debounce(getData2, 1000));
 
+
+// Throttling way
+// function throttler(callback, duration) {
+//     var start = true;
+//     var waitingArg = null;
+
+//     const timeOutFun = () => {
+//         if (waitingArg == null) {
+//             start = true;
+//         }
+//         else {
+//             callback(waitingArg);
+//             waitingArg = null;
+//             setTimeout(timeOutFun, duration);
+//         }
+//     }
+
+//     return (e) => {
+//         if (start) {
+//             callback(e);
+//             start = false;
+//         }
+
+//         waitingArg = e;
+    
+//         setTimeout(timeOutFun, duration);
+//     }
+// }
+
+function throttler(callback, duration) {
+    var shouldWait = false;
+    var waitingArgs = null;
+
+    const timeOutFun = () => {
+        if (waitingArgs == null) {
+            shouldWait = false;
+        }
+        else {
+            callback(waitingArgs);
+            waitingArgs = null;
+            setTimeout(timeOutFun, duration);
+        }
+    }
+
+    return (e) => {
+        if (shouldWait) {
+            waitingArgs = e;
+            return;
+        }
+        
+        callback(e);
+        shouldWait = true;
+    
+        setTimeout(timeOutFun, duration);
+    }
+}
+
+function getData3(e) {
+    document.querySelector("#third").textContent = e.target.value;
+}
+
+var specailFun2 = throttler(getData3, 2000);
+
+input.addEventListener("keyup", specailFun2);
