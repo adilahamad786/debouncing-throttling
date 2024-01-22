c8d1cc25e70c34f5100013cff96d5ac49a446801
= <=>
2024-01-23 00:07:43 +0530

    basic way to handle input by event linstener.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("working..")
+var input = document.querySelector("input");
+
+// Normal way
+function getData1(e) {
+    document.querySelector("#first").textContent = e.target.value;
+}
+input.addEventListener("keyup", getData1);
