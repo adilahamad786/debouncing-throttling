1d988a42fa76070ef5d746ba3571a7fe6fdad428
= <=>
2024-01-23 00:09:26 +0530

    Implement Debouncing concept.

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function getData1(e) {
     document.querySelector("#first").textContent = e.target.value;
 }
 input.addEventListener("keyup", getData1);
+
+
+
+// Debouncing way
+function debounce(callback, minimumDelay) {
+    var id;
+    return (e) => {
+        clearTimeout(id);
+
+        id = setTimeout(() => {
+            callback(e);
+        }, minimumDelay);
+    }
+}
+
+
+function getData2(e) {
+    document.querySelector("#second").textContent = e.target.value;
+}
+
+var specailFun1 = debounce(getData2, 1000);
+
+input.addEventListener("keyup", debounce(getData2, 1000));
+
